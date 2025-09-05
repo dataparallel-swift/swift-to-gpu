9e7641fe4f54297444899312f78eec00fd6f29b4
Trevor L. McDonell <trevor@passivelogic.com>
2025-09-05 10:16:38 +0200

    fix: use .initializeElement(at:to:) instead of subscript set

--- Sources/swift-to-ptx/Orphans/Array+init.swift
+++ Sources/swift-to-ptx/Orphans/Array+init.swift
@@ -13,22 +13,14 @@ extension Array {
     @usableFromInline
     init(count: Int, generator: @escaping (Int) -> Element) {
         precondition(count >= 0, "arrays must have non-negative sizes")
-        self.init()
-        self.reserveCapacity(count)
-        for i in 0..<count {
-            self.append(generator(i))
-        }
-
-        // In principle this code should also work, but we are getting SIGSEGV in
-        // swift_release(), atomic.c:265 for counts > 5. No idea why this is.
-        // self.init(
-        //     unsafeUninitializedCapacity: count,
-        //     initializingWith: { buffer, initializedCount in
-        //         for i in 0..<count {
-        //             buffer[i] = generator(i)
-        //         }
-        //         initializedCount = count
-        //     })
+        self.init(
+            unsafeUninitializedCapacity: count,
+            initializingWith: { buffer, initializedCount in
+                for i in 0..<count {
+                    buffer.initializeElement(at: i, to: generator(i))
+                }
+                initializedCount = count
+            })
     }
 }
 
