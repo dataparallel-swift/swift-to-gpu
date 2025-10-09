183190e9ca7e1c7991ce7bac3b3973bf1c4b2875
Trevor L. McDonell <trevor@passivelogic.com>
2025-10-09 15:33:16 +0200

    chore: swift lint

--- Sources/swift-to-ptx/Event.swift
+++ Sources/swift-to-ptx/Event.swift
@@ -1,29 +1,38 @@
+// Copyright (c) 2025 PassiveLogic, Inc.
+
 import CUDA
-import Tracy
 import Logging
 import SwiftToPTX_cbits
+import Tracy
 
 private let logger = Logger(label: "Event")
 
+/// An event in the current CUDA Context, used to signal when something like a
+/// kernel launch completes or takes place
 public final class Event {
-    internal var rawEvent : CUevent
+    internal var rawEvent: CUevent
     // TODO: we should probably keep track of which context this event is
     // associated with; there doesn't seem a way to query this via the CUDA API.
 
+    /// Create a new event with the given flags
+    /// https://docs.nvidia.com/cuda/archive/12.6.3/cuda-driver-api/group__CUDA__EVENT.html#group__CUDA__EVENT_1g450687e75f3ff992fe01662a43d9d3db
     public init(withFlags: [CUevent_flags] = [CU_EVENT_DISABLE_TIMING]) {
         let __zone = #Zone
         defer { __zone.end() }
 
         // See note in Stream.init()
-        var tmp : CUevent? = nil
+        var tmp: CUevent? = nil
 
         // Assumes we have an active context
         cuda_safe_call{cuEventCreate(&tmp, withFlags.reduce(0, {$0 | $1.rawValue}))}
 
-        self.rawEvent = tmp!    // cuEventCreate will error before this is nil
+        // cuEventCreate will error before this is nil
+        // swiftlint:disable:next force_unwrapping
+        self.rawEvent = tmp!
         logger.trace(".init(withFlags: \(withFlags)) -> \(self.rawEvent)")
     }
 
+    /// Import a raw CUDA 'CUevent'
     public init(rawEvent: CUevent) {
         let __zone = #Zone
         defer { __zone.end() }
@@ -32,7 +41,8 @@ public final class Event {
         logger.trace(".init(rawEvent: \(self.rawEvent))")
     }
 
-    // Wait for this event to be completed. This is a blocking call.
+    /// Wait for this event to be completed. This is a blocking call.
+    /// https://docs.nvidia.com/cuda/archive/12.6.3/cuda-driver-api/group__CUDA__EVENT.html#group__CUDA__EVENT_1g9e520d34e51af7f5375610bca4add99c
     public func sync() {
         let __zone = #Zone
         defer { __zone.end() }
@@ -40,7 +50,8 @@ public final class Event {
         cuda_safe_call{cuEventSynchronize(self.rawEvent)}
     }
 
-    // Returns 'true' if this event is complete
+    /// Returns 'true' if this event is complete
+    /// https://docs.nvidia.com/cuda/archive/12.6.3/cuda-driver-api/group__CUDA__EVENT.html#group__CUDA__EVENT_1g6f0704d755066b0ee705749ae911deef
     public func complete() -> Bool {
         let __zone = #Zone
         defer { __zone.end() }
@@ -50,11 +61,12 @@ public final class Event {
             case CUDA_SUCCESS: true
             case CUDA_ERROR_NOT_READY: false
             default: { () -> Bool in
-                var name : UnsafePointer<CChar>? = nil
-                var desc : UnsafePointer<CChar>? = nil
+                var name: UnsafePointer<CChar>? = nil
+                var desc: UnsafePointer<CChar>? = nil
                 cuGetErrorName(status, &name)
                 cuGetErrorString(status, &desc)
-                fatalError("CUDA call failed with error \(String.init(cString: name!)) (\(status.rawValue)): \(String.init(cString: desc!))")
+                // swiftlint:disable:next no_fatalerror force_unwrapping
+                fatalError("CUDA call failed with error \(String(cString: name!)) (\(status.rawValue)): \(String(cString: desc!))")
             }()
         }
         return result
@@ -68,4 +80,3 @@ public final class Event {
         cuda_safe_call{cuEventDestroy_v2(self.rawEvent)}
     }
 }
-
