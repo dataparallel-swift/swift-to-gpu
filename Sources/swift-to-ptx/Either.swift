7a32c1a3ebfc8f7a323cc43140e0aaec5d9ea977
Trevor L. McDonell <trevor@passivelogic.com>
2025-10-09 15:21:20 +0200

    chore: swift lint

--- Sources/swift-to-ptx/Either.swift
+++ Sources/swift-to-ptx/Either.swift
@@ -1,12 +1,15 @@
+// Copyright (c) 2025 PassiveLogic, Inc.
 
+// swiftlint:disable identifier_name comma
 
 /// The 'Either' type represents values with two possibilities: a value of type
 /// 'Either<A, B>' is either '.left(A)' or '.right(B)'.
 ///
-public enum Either<A,B> {
+public enum Either<A, B> {
     case left(A)
     case right(B)
 
+    /// Returns true if the value is a 'left' value, false otherwise
     public var isLeft: Bool {
         switch self {
             case .left: return true
@@ -14,6 +17,7 @@ public enum Either<A,B> {
         }
     }
 
+    /// Returns true if the value is a 'right' value, false otherwise
     public var isRight: Bool {
         switch self {
             case .left: return false
@@ -44,4 +48,3 @@ extension Either: Comparable where A: Comparable, B: Comparable {
         }
     }
 }
-
