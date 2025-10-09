7ff26dc252e60a76c2db19cf6d77e773f4c8b082
Trevor L. McDonell <trevor@passivelogic.com>
2025-10-09 17:44:19 +0200

    chore: swift format

--- Sources/swift-to-ptx/Util.swift
+++ Sources/swift-to-ptx/Util.swift
@@ -1,3 +1,5 @@
+// Copyright (c) 2025 PassiveLogic, Inc.
+
 import CUDA
 
 @usableFromInline
@@ -8,6 +10,7 @@ func cuda_safe_call(file: StaticString = #file, line: UInt = #line, _ function: 
         var desc: UnsafePointer<CChar>? = nil
         cuGetErrorName(result, &name)
         cuGetErrorString(result, &desc)
+        // swiftformat:disable wrap wrapArguments
         // swiftlint:disable:next force_unwrapping no_fatalerror
         fatalError("CUDA call failed with error \(String(cString: name!)) (\(result.rawValue)): \(String(cString: desc!))", file: file, line: line)
     }
