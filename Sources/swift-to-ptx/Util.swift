d45399885a2623dca98de8f590cc0ac3b0f4c9ca
Trevor L. McDonell <trevor@passivelogic.com>
2025-10-09 17:44:05 +0200

    chore: swift lint

--- Sources/swift-to-ptx/Util.swift
+++ Sources/swift-to-ptx/Util.swift
@@ -1,15 +1,14 @@
 import CUDA
 
 @usableFromInline
-func cuda_safe_call(file: StaticString = #file, line: UInt = #line, _ function: () -> CUresult) -> ()
-{
+func cuda_safe_call(file: StaticString = #file, line: UInt = #line, _ function: () -> CUresult) {
     let result = function()
     guard CUDA_SUCCESS == result else {
-        var name : UnsafePointer<CChar>? = nil
-        var desc : UnsafePointer<CChar>? = nil
+        var name: UnsafePointer<CChar>? = nil
+        var desc: UnsafePointer<CChar>? = nil
         cuGetErrorName(result, &name)
         cuGetErrorString(result, &desc)
-        fatalError("CUDA call failed with error \(String.init(cString: name!)) (\(result.rawValue)): \(String.init(cString: desc!))", file: file, line: line)
+        // swiftlint:disable:next force_unwrapping no_fatalerror
+        fatalError("CUDA call failed with error \(String(cString: name!)) (\(result.rawValue)): \(String(cString: desc!))", file: file, line: line)
     }
 }
-
