9bd1e8360ab6e9d3e90d59ac56b2f5d3e2ca8819
Trevor L. McDonell <trevor@passivelogic.com>
2025-10-09 15:23:51 +0200

    chore: swift format

--- Sources/swift-to-ptx/Either.swift
+++ Sources/swift-to-ptx/Either.swift
@@ -1,5 +1,6 @@
 // Copyright (c) 2025 PassiveLogic, Inc.
 
+// swiftformat:disable consecutiveSpaces
 // swiftlint:disable identifier_name comma
 
 /// The 'Either' type represents values with two possibilities: a value of type
@@ -12,7 +13,7 @@ public enum Either<A, B> {
     /// Returns true if the value is a 'left' value, false otherwise
     public var isLeft: Bool {
         switch self {
-            case .left: return true
+            case .left:  return true
             case .right: return false
         }
     }
@@ -20,7 +21,7 @@ public enum Either<A, B> {
     /// Returns true if the value is a 'right' value, false otherwise
     public var isRight: Bool {
         switch self {
-            case .left: return false
+            case .left:  return false
             case .right: return true
         }
     }
@@ -30,9 +31,9 @@ extension Either: Equatable where A: Equatable, B: Equatable {
     @inlinable
     public static func == (lhs: Self, rhs: Self) -> Bool {
         switch (lhs, rhs) {
-            case (.left(let a),  .left(let b)):  return a == b
-            case (.right(let a), .right(let b)): return a == b
-            default:                             return false
+            case let (.left(a),  .left(b)):  return a == b
+            case let (.right(a), .right(b)): return a == b
+            default: return false
         }
     }
 }
@@ -41,10 +42,10 @@ extension Either: Comparable where A: Comparable, B: Comparable {
     @inlinable
     public static func < (lhs: Self, rhs: Self) -> Bool {
         switch (lhs, rhs) {
-            case (.left(let a),  .left(let b)):  return a < b
-            case (.left,         .right):        return true
-            case (.right,        .left):         return false
-            case (.right(let a), .right(let b)): return a < b
+            case let (.left(a),  .left(b)):  return a < b
+            case     (.left,     .right):    return true
+            case     (.right,    .left):     return false
+            case let (.right(a), .right(b)): return a < b
         }
     }
 }
