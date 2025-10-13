8da260dc30163d36f22898870f59bc15111fd568
Trevor L. McDonell <trevor@passivelogic.com>
2025-10-13 13:55:11 +0200

    chore: swift format

--- Sources/swift-to-ptx/Prelude.swift
+++ Sources/swift-to-ptx/Prelude.swift
@@ -1,3 +1,5 @@
+// Copyright (c) 2025 PassiveLogic, Inc.
+
 // swiftlint:disable identifier_name
 
 // NOTE [Annotations that are required for public-facing functions]:
@@ -32,8 +34,7 @@ import Logging
 /// Construct a new array by applying a function at each index
 @inline(__always)
 @_alwaysEmitIntoClient
-public func generate<A, Err: Error>(count: Int, stream s: Stream = streamPerThread, _ f: (Int) throws(Err) -> A) throws(Err) -> Array<A>
-{
+public func generate<A, Err: Error>(count: Int, stream s: Stream = streamPerThread, _ f: (Int) throws(Err) -> A) throws(Err) -> Array<A> {
     var xs = Array<A>(unsafeUninitializedCapacity: count)
     try generate(into: &xs, stream: s, f)
     return xs
@@ -42,9 +43,8 @@ public func generate<A, Err: Error>(count: Int, stream s: Stream = streamPerThre
 /// Regenerate an existing array by applying a function at each index
 @inline(__always)
 @_alwaysEmitIntoClient
-public func generate<A, Err: Error>(into xs: inout Array<A>, stream s: Stream = streamPerThread, _ f: (Int) throws(Err) -> A) throws(Err)
-{
-    try parallel_for(iterations: xs.count, stream: s) { i throws (Err) in
+public func generate<A, Err: Error>(into xs: inout Array<A>, stream s: Stream = streamPerThread, _ f: (Int) throws(Err) -> A) throws(Err) {
+    try parallel_for(iterations: xs.count, stream: s) { i throws(Err) in
         xs[i] = try f(i)
     }.sync()
 }
@@ -52,16 +52,14 @@ public func generate<A, Err: Error>(into xs: inout Array<A>, stream s: Stream = 
 /// Construct a new array where all elements have the same element
 @inline(__always)
 @_alwaysEmitIntoClient
-public func fill<A>(count: Int, with x: A, stream s: Stream = streamPerThread) -> Array<A>
-{
+public func fill<A>(count: Int, with x: A, stream s: Stream = streamPerThread) -> Array<A> {
     generate(count: count, stream: s) { _ in x }
 }
 
 /// Set all elements of an array to the given value
 @inline(__always)
 @_alwaysEmitIntoClient
-public func fill<A>(into xs: inout Array<A>, with x: A, stream s: Stream = streamPerThread)
-{
+public func fill<A>(into xs: inout Array<A>, with x: A, stream s: Stream = streamPerThread) {
     generate(into: &xs, stream: s) { _ in x }
 }
 
@@ -74,8 +72,7 @@ public func fill<A>(into xs: inout Array<A>, with x: A, stream s: Stream = strea
 ///
 @inline(__always)
 @_alwaysEmitIntoClient
-public func map<A, B, Err: Error>(_ xs: Array<A>, stream s: Stream = streamPerThread, _ f: (A) throws(Err) -> B) throws(Err) -> Array<B>
-{
+public func map<A, B, Err: Error>(_ xs: Array<A>, stream s: Stream = streamPerThread, _ f: (A) throws(Err) -> B) throws(Err) -> Array<B> {
     try imap(xs, stream: s) { _, x throws(Err) in try f(x) }
 }
 
@@ -84,8 +81,12 @@ public func map<A, B, Err: Error>(_ xs: Array<A>, stream s: Stream = streamPerTh
 /// updated.
 @inline(__always)
 @_alwaysEmitIntoClient
-public func map<A, B, Err: Error>(_ xs: Array<A>, into ys: inout Array<B>, stream s: Stream = streamPerThread, _ f: (A) throws(Err) -> B) throws(Err)
-{
+public func map<A, B, Err: Error>(
+    _ xs: Array<A>,
+    into ys: inout Array<B>,
+    stream s: Stream = streamPerThread,
+    _ f: (A) throws(Err) -> B
+) throws(Err) {
     try imap(xs, into: &ys, stream: s) { _, x throws(Err) in try f(x) }
 }
 
@@ -93,8 +94,11 @@ public func map<A, B, Err: Error>(_ xs: Array<A>, into ys: inout Array<B>, strea
 /// array with its index.
 @inline(__always)
 @_alwaysEmitIntoClient
-public func imap<A, B, Err: Error>(_ xs: Array<A>, stream s: Stream = streamPerThread, _ f: (Int, A) throws(Err) -> B) throws(Err) -> Array<B>
-{
+public func imap<A, B, Err: Error>(
+    _ xs: Array<A>,
+    stream s: Stream = streamPerThread,
+    _ f: (Int, A) throws(Err) -> B
+) throws(Err) -> Array<B> {
     var ys = Array<B>(unsafeUninitializedCapacity: xs.count)
     try imap(xs, into: &ys, stream: s, f)
     return ys
@@ -105,8 +109,12 @@ public func imap<A, B, Err: Error>(_ xs: Array<A>, stream s: Stream = streamPerT
 /// be updated.
 @inline(__always)
 @_alwaysEmitIntoClient
-public func imap<A, B, Err: Error>(_ xs: Array<A>, into ys: inout Array<B>, stream s: Stream = streamPerThread, _ f: (Int, A) throws(Err) -> B) throws(Err)
-{
+public func imap<A, B, Err: Error>(
+    _ xs: Array<A>,
+    into ys: inout Array<B>,
+    stream s: Stream = streamPerThread,
+    _ f: (Int, A) throws(Err) -> B
+) throws(Err) {
     let n = min(xs.count, ys.count)
 
     try parallel_for(iterations: n, stream: s) { i throws(Err) in
@@ -119,8 +127,12 @@ public func imap<A, B, Err: Error>(_ xs: Array<A>, into ys: inout Array<B>, stre
 /// of the two source arrays.
 @inline(__always)
 @_alwaysEmitIntoClient
-public func zipWith<A, B, C, Err: Error>(_ xs: Array<A>, _ ys: Array<B>, stream s: Stream = streamPerThread, _ f: (A, B) throws(Err) -> C) throws(Err) -> Array<C>
-{
+public func zipWith<A, B, C, Err: Error>(
+    _ xs: Array<A>,
+    _ ys: Array<B>,
+    stream s: Stream = streamPerThread,
+    _ f: (A, B) throws(Err) -> C
+) throws(Err) -> Array<C> {
     try izipWith(xs, ys, stream: s) { _, x, y throws(Err) in try f(x, y) }
 }
 
@@ -128,8 +140,13 @@ public func zipWith<A, B, C, Err: Error>(_ xs: Array<A>, _ ys: Array<B>, stream 
 /// source arrays. This function has intersection semantics.
 @inline(__always)
 @_alwaysEmitIntoClient
-public func zipWith<A, B, C, Err: Error>(_ xs: Array<A>, _ ys: Array<B>, into zs: inout Array<C>, stream s: Stream = streamPerThread, _ f: (A, B) throws(Err) -> C) throws(Err)
-{
+public func zipWith<A, B, C, Err: Error>(
+    _ xs: Array<A>,
+    _ ys: Array<B>,
+    into zs: inout Array<C>,
+    stream s: Stream = streamPerThread,
+    _ f: (A, B) throws(Err) -> C
+) throws(Err) {
     try izipWith(xs, ys, into: &zs, stream: s) { _, x, y throws(Err) in try f(x, y) }
 }
 
@@ -138,8 +155,12 @@ public func zipWith<A, B, C, Err: Error>(_ xs: Array<A>, _ ys: Array<B>, into zs
 /// intersection semantics.
 @inline(__always)
 @_alwaysEmitIntoClient
-public func izipWith<A, B, C, Err: Error>(_ xs: Array<A>, _ ys: Array<B>, stream s: Stream = streamPerThread, _ f: (Int, A, B) throws(Err) -> C) throws(Err) -> Array<C>
-{
+public func izipWith<A, B, C, Err: Error>(
+    _ xs: Array<A>,
+    _ ys: Array<B>,
+    stream s: Stream = streamPerThread,
+    _ f: (Int, A, B) throws(Err) -> C
+) throws(Err) -> Array<C> {
     let n  = min(xs.count, ys.count)
     var zs = Array<C>(unsafeUninitializedCapacity: n)
     try izipWith(xs, ys, into: &zs, stream: s, f)
@@ -151,8 +172,13 @@ public func izipWith<A, B, C, Err: Error>(_ xs: Array<A>, _ ys: Array<B>, stream
 /// intersection semantics.
 @inline(__always)
 @_alwaysEmitIntoClient
-public func izipWith<A, B, C, Err: Error>(_ xs: Array<A>, _ ys: Array<B>, into zs: inout Array<C>, stream s: Stream = streamPerThread, _ f: (Int, A, B) throws(Err) -> C) throws(Err)
-{
+public func izipWith<A, B, C, Err: Error>(
+    _ xs: Array<A>,
+    _ ys: Array<B>,
+    into zs: inout Array<C>,
+    stream s: Stream = streamPerThread,
+    _ f: (Int, A, B) throws(Err) -> C
+) throws(Err) {
     let n = min(xs.count, min(ys.count, zs.count))
 
     return try parallel_for(iterations: n, stream: s) { i throws(Err) in
@@ -197,8 +223,12 @@ public func izipWith<A, B, C, Err: Error>(_ xs: Array<A>, _ ys: Array<B>, into z
 /// ```
 @inline(__always)
 @_alwaysEmitIntoClient
-public func permute<A, Err: Error>(from: Array<A>, into: inout Array<A>, combining f: (A, A) throws(Err) -> A, _ p: (Int) throws(Err) -> Int?) throws(Err)
-{
+public func permute<A, Err: Error>(
+    from: Array<A>,
+    into: inout Array<A>,
+    combining f: (A, A) throws(Err) -> A,
+    _ p: (Int) throws(Err) -> Int?
+) throws(Err) {
     typealias Lock = UInt32.AtomicRepresentation
     var locks: Array<Lock> = fill(count: from.count, with: .init(0))
 
@@ -237,8 +267,7 @@ public func permute<A, Err: Error>(from: Array<A>, into: inout Array<A>, combini
 /// function is not surjective) then the result is non-deterministic.
 @inline(__always)
 @_alwaysEmitIntoClient
-public func permute<A, Err: Error>(from: Array<A>, into: inout Array<A>, _ p: (Int) throws(Err) -> Int?) throws(Err)
-{
+public func permute<A, Err: Error>(from: Array<A>, into: inout Array<A>, _ p: (Int) throws(Err) -> Int?) throws(Err) {
     try parallel_for(iterations: from.count) { i throws(Err) in
         if let j = try p(i) {
             into[j] = from[i]
@@ -257,8 +286,7 @@ public func permute<A, Err: Error>(from: Array<A>, into: inout Array<A>, _ p: (I
 /// source array.
 @inline(__always)
 @_alwaysEmitIntoClient
-public func backpermute<A, Err: Error>(from: Array<A>, count: Int, _ p: (Int) throws(Err) -> Int) throws(Err) -> Array<A>
-{
+public func backpermute<A, Err: Error>(from: Array<A>, count: Int, _ p: (Int) throws(Err) -> Int) throws(Err) -> Array<A> {
     var into = Array<A>(unsafeUninitializedCapacity: count)
     try backpermute(from: from, into: &into, p)
     return into
@@ -268,8 +296,7 @@ public func backpermute<A, Err: Error>(from: Array<A>, count: Int, _ p: (Int) th
 /// from the source array according to the permutation function.
 @inline(__always)
 @_alwaysEmitIntoClient
-public func backpermute<A, Err: Error>(from: Array<A>, into: inout Array<A>, _ p: (Int) throws(Err) -> Int) throws(Err)
-{
+public func backpermute<A, Err: Error>(from: Array<A>, into: inout Array<A>, _ p: (Int) throws(Err) -> Int) throws(Err) {
     try parallel_for(iterations: into.count) { i throws(Err) in
         into[i] = from[try p(i)]
     }.sync()
@@ -280,8 +307,7 @@ public func backpermute<A, Err: Error>(from: Array<A>, into: inout Array<A>, _ p
 /// constant value used as a default)
 @inline(__always)
 @_alwaysEmitIntoClient
-public func backpermute<A, Err: Error>(from: Array<A>, count: Int, _ p: (Int) throws(Err) -> Either<Int, A>) throws(Err) -> Array<A>
-{
+public func backpermute<A, Err: Error>(from: Array<A>, count: Int, _ p: (Int) throws(Err) -> Either<Int, A>) throws(Err) -> Array<A> {
     var into = Array<A>(unsafeUninitializedCapacity: count)
     try backpermute(from: from, into: &into, p)
     return into
@@ -293,12 +319,11 @@ public func backpermute<A, Err: Error>(from: Array<A>, count: Int, _ p: (Int) th
 /// used as a default).
 @inline(__always)
 @_alwaysEmitIntoClient
-public func backpermute<A, Err: Error>(from: Array<A>, into: inout Array<A>, _ p: (Int) throws(Err) -> Either<Int, A>) throws(Err)
-{
+public func backpermute<A, Err: Error>(from: Array<A>, into: inout Array<A>, _ p: (Int) throws(Err) -> Either<Int, A>) throws(Err) {
     try parallel_for(iterations: into.count) { i throws(Err) in
         let v = switch try p(i) {
-            case .left(let j): from[j]
-            case .right(let v): v
+            case let .left(j): from[j]
+            case let .right(v): v
         }
         into[i] = v
     }.sync()
@@ -340,22 +365,21 @@ public func parallel_for<E: Error>
     allocator: CachingHostAllocator = smallBlockAllocator,
     stream: Stream = streamPerThread,
     _ body: (Int) throws(E) -> Void
-) throws(E) -> Event
-{
+) throws(E) -> Event {
     // Initialise and use the logger directly inline in order to avoid needing
     // to mark it as usableFromInline, as that causes conflicts with the loggers
     // defined in other modules (and which are actually used).
     let logger = Logger(label: "")
     logger.warning("""
-        *** WARNING *** parallel_for loop executing on the host!
-        Compile in release mode to enable PTX translation. Failing that, please submit a bug to: https://gitlab.com/PassiveLogic/compiler/swift-to-ptx/-/issues
-        """)
+    *** WARNING *** parallel_for loop executing on the host!
+    Compile in release mode to enable PTX translation. Failing that, please submit a bug to: https://gitlab.com/PassiveLogic/compiler/swift-to-ptx/-/issues
+    """)
 
     dontLetTheCompilerOptimizeThisAway(context)
     dontLetTheCompilerOptimizeThisAway(allocator)
     dontLetTheCompilerOptimizeThisAway(stream)
 
-    for i in 0..<iterations {
+    for i in 0 ..< iterations {
         try body(i)
     }
 
