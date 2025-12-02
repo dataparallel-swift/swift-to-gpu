bba87bc0607e554d460bbe395ee1ae7c2610616a
Trevor L. McDonell <trevor@passivelogic.com>
2025-12-02 15:50:05 +0100

    chore: delete icebox

--- Backends/PTXBackend/icebox/Tests/Simple.swift
+++
@@ -1,9 +0,0 @@
-import Randy
-import XCTest
-
-// Why can't I manually specify the test hierarchy? What a fucking joke...
-class Simple : XCTestCase {
-    let sizeRange : ClosedRange<Int> = 1...4096
-    var generator : SeedableRandomNumberGenerators.RecommendedGenerator = .init()
-}
-
